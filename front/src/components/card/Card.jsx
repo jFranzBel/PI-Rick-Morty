c8dff65ddbfc74862a585289009b9a3bf4e28c02
Jonnathan Franz <jonnathan.franz.87@outlook.com>
2024-04-22 15:52:57 -0600

    Update Card.jsx

--- front/src/components/card/Card.jsx
+++ front/src/components/card/Card.jsx
@@ -6,43 +6,39 @@ import { addFav, removeFav } from '../../redux/actions';
 
 export default function Card(props) {
 
- const dispatch = useDispatch();
- const myFavorites = useSelector( state => state.myFavorites);
- const [ isFav, setIsFav ] = useState(false);
+    const dispatch = useDispatch();
+    const myFavorites = useSelector(state => state.myFavorites);
+    const [isFav, setIsFav] = useState(false);
 
- const handleFavorite = () => {
-  if (isFav) {
-    setIsFav(false);
-    dispatch(removeFav(props.id));
-  } else {
-    setIsFav(true);
-    dispatch(addFav(props)); // Pass the character object
-  }
-};
+    const handleFavorite = () => {
+        if (isFav) {
+            setIsFav(false);
+            dispatch(removeFav(props.id));
+        } else {
+            setIsFav(true);
+            dispatch(addFav(props)); // Pass the character object
+        }
+    };
 
-useEffect(() => {
-  const isFavorite = myFavorites.some((fav) => fav.id === props.id);
-  setIsFav(isFavorite);
-}, [myFavorites, props]);
+    useEffect(() => {
+        const isFavorite = myFavorites.some((fav) => fav.id === props.id);
+        setIsFav(isFavorite);
+    }, [myFavorites, props]);
 
- return (
-   <div className="card_container">
-     {
-       isFav ? (
-          <button className='like-button' onClick={ handleFavorite }>❤️</button>
-       ) : (
-          <button className='like-button' onClick={ handleFavorite }>🤍</button>
-       )
-     }
-     <button className='card_container_button' onClick={ props.onClose }>X</button>
-     <img src={ props.image } alt={ props.name } />
-     <Link to={ `/detail/${ props.id }` }>
-       <h2>{ props.name }</h2>
-     </Link>
-     <h2>{ props.status }</h2>
-     <h2>{ props.species }</h2>
-     <h2>{ props.gender }</h2>
-     <h2>{ props.origin }</h2>
-   </div>
- );
+    return (
+        <div className="card_container">
+            {
+                isFav ? (
+                    <button className='like-button' onClick={handleFavorite}>❤️</button>
+                ) : (
+                    <button className='like-button' onClick={handleFavorite}>🤍</button>
+                )
+            }
+            <button className='card_container_button' onClick={props.onClose}>X</button>
+            <Link to={`/detail/${props.id}`}>
+                <img src={props.image} alt={props.name} />
+                <h2>{props.name}</h2>
+            </Link>
+        </div>
+    );
 }
