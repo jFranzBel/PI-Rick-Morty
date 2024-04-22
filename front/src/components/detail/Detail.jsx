4ec477f0aa7e8b623be350e6e9a553415436a44f
Jonnathan Franz <jonnathan.franz.87@outlook.com>
2024-04-22 16:00:22 -0600

    Update Detail.jsx

--- front/src/components/detail/Detail.jsx
+++ front/src/components/detail/Detail.jsx
@@ -27,11 +27,11 @@ const Detail = () => {
             <img src={character?.image} alt={character?.name} />
             <br />
             <br />
-            <h1>{character?.name}</h1>
-            <p>{character?.status}</p>
-            <p>{character?.species}</p>
-            <p>{character?.gender}</p>
-            <p>{character?.origin?.name}</p>
+            <h1>Name - {character?.name}</h1>
+            <p>Status - {character?.status}</p>
+            <p>Species - {character?.species}</p>
+            <p>Gender - {character?.gender}</p>
+            <p>Origin - {character?.origin?.name}</p>
         </div>
     )
 };
